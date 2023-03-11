3e1c85ef4ec30462eb471156c00585f1c82c08ab
Poojachakor <chakorpooja07@gmail.com>
2023-03-11 10:30:17 +0530

    Add: DataStructureDemo

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StackUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
